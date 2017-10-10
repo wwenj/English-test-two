b8f2a4afd1dc7c0548aebe6d24799e22e693a7fa
wwenj <man@wwenj.com>
2017-10-10 23:13:18 +0800

    17.10.10提交

--- js/main.js
+++ js/main.js
@@ -15,8 +15,8 @@ $(function(){
         methods:{
             getQuestionList:function(){
             	$.ajax({
-                    url: "http://test.zhituteam.com/index.php/home/api/getquestion",
-                    // url: "./json/first.json",
+                    // url: "http://test.zhituteam.com/index.php/home/api/getquestion",
+                    url: "./json/first.json",
                     type: "get",
                     dataType: "json",
                     data:{
@@ -39,6 +39,7 @@ $(function(){
                 this.startShow=true;
                 this.contentShow=false;
             },
+
             /*点击变换按钮外形*/
             playChage:function(playId,audioId,index){   //点击节点，音频节点，当前点击的index
                 var playTop=$(playId);
@@ -51,8 +52,9 @@ $(function(){
             },
             fourOptionClick:function(index){
                 $('.fourOption').eq(index).css('border','1px solid #59ba77').siblings().css('border','1px solid rgba(255,255,255,0)')
-                this.annId=index;
+                this.annId=index;   //当前做出的选项
             },
+
             /*点击左右*/
             prveClick:function(){
                 if (this.listsId==1857){
@@ -63,8 +65,15 @@ $(function(){
                 }
             },
             nextClick:function(){
-                this.listsId+=1;
-                alert(this.annId)
+                this.listsId+=1;    //页码加1
+                var inidexId=this.listsId-1858;     //当前题目页数，对应当前lists索引
+                if(this.questionList[inidexId].data.stem[this.annId].istrue==1){
+                    alert('正确')
+                }else{
+                    alert('错误')
+                }
+
+
             }
 
 		}
